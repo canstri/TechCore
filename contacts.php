328f6d5c470d256f2a78a7192f6146b721f0cef4
Kuanysh <kuanyshoteu@gmail.com>
2018-04-04 19:02:37 +0600

    betta

---
+++ contacts.php
@@ -0,0 +1,188 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<title>Tech Core</title>
+	
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/bootstrap.css" />
+
+	<!--=== Other CSS files ===-->
+	<link rel="stylesheet" href="css/animate.css" />
+	<link rel="stylesheet" href="css/jquery.vegas.css" />
+	
+	<!--=== Main Stylesheets ===-->
+	<link rel="stylesheet" href="css/base.css" />
+	<link rel="stylesheet" href="css/style.css" />
+	<link rel="stylesheet" href="css/responsive.css" />
+	<link rel="stylesheet" href="css/call-back.css" />
+	
+	<!--=== Color Scheme, three colors are available red.css, orange.css and gray.css ===-->
+	<link rel="stylesheet" id="scheme-source" href="css/schemes/gray.css" />
+
+	<link href="https://fonts.googleapis.com/css?family=Bitter|Mukta+Malar" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		
+</head>
+<body>
+	<div id="myfond_gris" opendiv=""></div>
+	<a href=""> 
+		<div iddiv="box_1" class="mymagicoverbox">
+			Обратный звонок
+		</div>
+	 </a>
+	<div id="box_1" class="mymagicoverbox_fenetre" style="left:-225px; width:450px;">
+		Заказать обратный звонок
+		<div class="mymagicoverbox_fenetreinterieur" style="height:200px; ">
+			<div align="center">
+				<form action="" method="post">
+				  <div class="form-group">
+				    <label>Ваш номер телефона:</label>
+				    <input type="text" class="form-control" name="phone" placeholder="Номер телефона">
+				    <small id="emailHelp" class="form-text text-muted">Ваш номер телефона не будет передан другим лицам.</small>
+				  </div>
+				  <button type="submit" class="btn btn-primary">Заказать</button>
+				  
+				</form>
+			</div>
+		</div>
+	</div>
+	<?php
+		if(isset($_POST['submit'])){
+			$phone = $_POST['phone'];
+			$mymail = 'ukuanysh@mail.ru';
+			$headers = "Reply-to:$phone";
+			mail($mymail, $headers);
+		}
+	?>
+ 
+<link href='http://fonts.googleapis.com/css?family=Roboto:100,400,300,500,700' rel='stylesheet' type='text/css'>
+ 
+ 
+	<!--=== Header section Starts ===-->
+	<div id="header" class="header-section">
+		<!-- sticky-bar Starts-->
+		<div class="sticky-bar-wrap">
+			<div class="sticky-section">
+				<div id="topbar-hold" class="nav-hold container">
+					<nav class="navbar" role="navigation">
+						<div class="navbar-header center">
+							<!--=== Site Name ===-->
+							<a class="site-name" href="index.php">TECHCORE</a>
+							<p class="cdo">Центр дополнительного образования</p>
+						</div>
+						
+						<!-- Main Navigation menu Starts -->
+						<div class="collapse navbar-collapse navbar-responsive-collapse">
+							<ul class="nav navbar-nav navbar-right">
+								<li ><a href="index.php">Главная</a></li>
+								<li><a href="index.php">Курсы</a></li>
+								<li><a href="index.php">Преимущества</a></li>
+								<li class="current"><a href="contacts.php">Контакты</a></li>
+							</ul>
+						</div>
+						<!-- Main Navigation menu ends-->
+					</nav>
+				</div>
+			</div>
+		</div>
+		<!-- sticky-bar Ends-->
+		<!--=== Header section Ends ===-->
+		
+	</div>
+	
+		<!--=== ScreenShots section Starts ===-->
+	<section id="section-pricing" class="screenshots-wrap">
+		<div class="container screenshots animated" data-animation="fadeInUp" data-animation-delay="400">
+			<div class="row porfolio-container">
+				<!-- Single screenshot starts -->
+				<div class="col-md-12">
+					<script src="https://maps.api.2gis.ru/2.0/loader.js"></script>
+					<div class="row">
+					            <div class="content" >
+					                <h1 class="black">Get in touch with us</h1>
+					                <h3 class="black">Ask what you want to know via information below</h3>
+					                <h3 class="black">Our location:</h3>
+					            <div class="content">
+					                <div id="map" style="width: 100%; height: 400px;">
+					                </div>
+					                <script>
+					                	var map;
+					                    DG.then(function() {
+					                        map = DG.map('map', {
+					                            center: [43.2366, 76.9126],
+					                            zoom: 16,
+
+					                        });
+					                        DG.marker([43.2366, 76.9126]).addTo(map).bindPopup('Мы находимся здесь!  TechCore');;
+					                    });
+					                </script>
+					            </div>
+					        </div>
+					    </div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--=== ScreenShots section Ends ===-->
+
+	
+	<!--=== Footer section Starts ===-->
+	<div id="section-footer" class="footer-wrap">
+		<div class="container footer center">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="left col-md-3 col-md-offset-1">
+						<a href="tel:5555555555" class="white"><i class="fas fa-phone"></i> +7 xxx xxxxxxxx</a>
+						<p style="margin-top: 20px;"><i class="fas fa-envelope"></i> techcore@mail.com</p>
+						<a href="contacts" class="white"><i style="margin-top: 20px;" class="fas fa-map-marker-alt"></i> Жандосова, 2</p>
+					</div>
+					<div class="social-icons col-md-4">
+						<h3 style="margin-top: -5px;">Мы в социальных сетях:</h3>
+						<ul>
+							<li><a href="#"><i style="margin-top: 20px;" class="fab fa-facebook-square"></i></a></li>
+							<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+							<li><a href="#"><i class="fab fa-google-plus-square"></i></a></li>
+							
+						</ul>
+					</div>
+					<div class="col-md-4">
+						<h3 style="margin-top: -5px;" class="footer-title"><!-- Footer Title -->
+						<a class="white" href="#header">TechCore</a>
+						</h3>
+					</div>
+					<!-- Social Links -->
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--=== Footer section Ends ===-->
+	
+<!--==== Js files ====-->
+<!--==== Essential files ====-->
+
+<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/bootstrapValidator.min.js"></script>
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+
+<!--==== Slider and Card style plugin ====-->
+<script type="text/javascript" src="js/jquery.baraja.js"></script>
+<script type="text/javascript" src="js/jquery.vegas.min.js"></script>
+<script type="text/javascript" src="js/jquery.bxslider.min.js"></script>
+
+<!--==== Scroll and navigation plugins ====-->
+<script type="text/javascript" src="js/jquery.nicescroll.min.js"></script>
+<script type="text/javascript" src="js/jquery.nav.js"></script>
+<script type="text/javascript" src="js/jquery.appear.js"></script>
+<script type="text/javascript" src="js/jquery.fitvids.js"></script>
+<script type="text/javascript" src="js/call-back.js"></script>
+
+
+<!--==== Custom Script files ====-->
+<script type="text/javascript" src="js/custom.js"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+
+</body>
+</html>
